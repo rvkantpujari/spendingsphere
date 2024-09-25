7e5aa7f946ecf740d70bbd27df0e06390c4da892
Ravi Kant Pujari <rvkantpujari@gmail.com>
2024-09-24 22:11:29 -0300

    add: project files

---
+++ README.md
@@ -0,0 +1,17 @@
+# SpendingSphere
+
+SpendingSphere is an intuitive web app that helps you effortlessly manage your income and expenses while providing insightful demographic analytics.
+
+## Getting Started
+
+First, install `npm` packages:
+
+```bash
+npm i
+```
+
+Next, run the development server:
+
+```bash
+npm run dev
+```
---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
+});
+const geistMono = localFont({
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
+});
+
+export const metadata: Metadata = {
+	title: "SpendingSphere",
+	description:
+		"SpendingSphere is an intuitive web app that helps you effortlessly manage your income and expenses while providing insightful demographic analytics. Easily enter data, upload CSV files, or connect to your banking institution for seamless financial tracking and visualization.",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
